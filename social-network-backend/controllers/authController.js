f394198f9b3512e9e438d6fd6c37be38d310c7af
timothee-piotre <155533950+timothee-piotre@users.noreply.github.com>
2024-08-27 10:48:24 +0200

    Update authController.js

--- social-network-backend/controllers/authController.js
+++ social-network-backend/controllers/authController.js
@@ -5,21 +5,22 @@ const jwt = require('jsonwebtoken');
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
+function register(){
+    exports.register = async (req, res) => {
+        const { username, email, password } = req.body;
 
-exports.register = async (req, res) => {
-    const { username, email, password } = req.body;
-
-    try {
-        const user = await User.create({ username, email, password });
-        res.status(201).json({
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            token: generateToken(user.id),
-        });
-    } catch (error) {
-        res.status(400).json({ message: 'User could not be created', error: error.message });
-    }
+        try {
+            const user = await User.create({ username, email, password });
+            res.status(201).json({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user.id),
+            });
+        } catch (error) {
+            res.status(400).json({ message: 'User could not be created', error: error.message });
+        }
+    };
 };
 
 exports.login = async (req, res) => {
